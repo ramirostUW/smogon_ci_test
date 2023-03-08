48e3adcf9235eac2c246d9bab5336a08eaa91828
ramirostuW <ramirost@uw.edu>
2023-03-07 17:46:07 -0800

    more changes

--- main.py
+++ main.py
@@ -9,14 +9,65 @@ app = FastAPI()
 @app.get("/", response_class=HTMLResponse)
 def root ():
     return """
+    <style>
+        .center {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+        }
+        body {
+        font-family: 'IBM Plex Sans', sans-serif;
+        color: white !important;
+        background-color: black !important;
+        }
+
+        h1 {
+        font-family: 'IBM Plex Mono', monospace;
+        font-weight: 600 !important;
+        text-align: center;
+        font-size:5vw !important;
+        }
+
+        h2 {
+        font-family: 'IBM Plex Mono', monospace;
+        font-weight: 600 !important;
+        text-align: center;
+        font-size:3vw;
+        }
+
+        a {
+        color: #46b966;
+        text-decoration: none !important;
+        }
+
+        a:hover {
+        color: #17B2B5 !important;
+        }
+
+        .logoPic {
+        width: 70%;
+        margin: auto;
+        display: block;
+        }
+
+        .header {
+        background-color: #0e2f44;
+        }
+
+        ul {
+        display: table;
+        margin: 0 auto;
+        }
+    </style>
     <h1>Smogon API</h1>
     <p>
-    <image src="https://www.dropbox.com/s/59sdrzril0s4mam/smogonBanner.jpg?dl=1" style="width:600px;height:300px;"></image><br />
+    <image src="https://www.dropbox.com/s/59sdrzril0s4mam/smogonBanner.jpg?dl=1" style="width:600px;height:300px;" class="center"></image><br />
     Welcome to the Smogon API Project! This project is aimed at allowing developers 
     to access all of the data on <a href="https://smogon.com">Smogon</a> using API-style endpoints, in order
     to perform analysis or present different views. 
     </p>
-    <p> You can find a list of the different endpoints that we have <a href="/docs"> here </a>. 
+    <p> You can find a list of the different endpoints that we have <a href="/docs">here</a>. 
     """
 
 @app.get("/charizard")
