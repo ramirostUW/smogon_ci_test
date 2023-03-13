0e1944e5cb2db2ae7f903059651713105c7d9f63
Jason Wang <guwa@amazon.com>
2023-03-12 17:26:16 -0700

    Create setup.py, restructure package etc.

---
+++ smogonapi/main.py
@@ -0,0 +1,22 @@
+# pylint:disable=unused-argument
+"""
+This is the root module of our Smogon API. It is a barebones file that
+mostly just grabs the endpoints from the appropriate submodules.
+"""
+
+import uvicorn
+from fastapi import FastAPI
+from smogonapi.submodules import smogon_endpoints, http_endpoints, kaggle_data_endpoints
+
+myApp = FastAPI()
+myApp.include_router(smogon_endpoints.router)
+myApp.include_router(http_endpoints.router)
+myApp.include_router(kaggle_data_endpoints.router)
+
+
+def main():
+    uvicorn.run(myApp)
+
+
+if __name__ == "__main__":
+    main()
